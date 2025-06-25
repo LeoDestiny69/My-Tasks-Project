14da050460bebd9d1b41de0412a6bdaec55b00c2
Tinnaphop <74139538+LeoDestiny69@users.noreply.github.com>
2025-06-26 00:23:03 +0700

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,16 @@
-// frontend/next.config.js
+// frontend/next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', 
-
   images: {
     unoptimized: true, 
   },
-
   experimental: {
     swcMinify: true,
     forceSwcTransforms: true,
   },
+  
+  basePath: '/My-Tasks-Project', 
 };
 
 export default nextConfig;
